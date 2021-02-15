95f162578022bdbee025b6ad001156f2b7d88a76
schein00 <schein.jefer@gmail.com>
2021-02-14 23:11:13 -0300

    teste with Scaffold

--- imcApp/lib/main.dart
+++ imcApp/lib/main.dart
@@ -17,6 +17,27 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Calculadore de IMC'),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(104, 34, 139, 1.0),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+        ],
+      ),
+      backgroundColor: Colors.grey,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline,
+              size: 140.0, color: Color.fromRGBO(104, 34, 139, 1.0)),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: 'Peso (KG)',
+                labelStyle:
+                    TextStyle(color: Color.fromRGBO(104, 34, 139, 1.0))),
+            textAlign: TextAlign.center,
+          )
+        ],
       ),
     );
   }
